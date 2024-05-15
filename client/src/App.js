eebc72939da4758806ec43495204d94ba81964f7
pwangy <blipsandclicks@gmail.com>
2024-05-15 09:58:33 -0700

    rm placeholder h3

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,16 @@ import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
 import { toast, ToastContainer, Bounce } from 'react-toastify'
 import { AuthContext } from './context/AuthContext'
-// import { ToastContext } from './context/ToastContext'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
 	const { user } = useContext(AuthContext)
-	// const { toastState } = useContext(ToastContext)
-
-
 
 	return (
 		<main>
 			<Header />
 			<ToastContainer role='alert' stacked />
-			<h3>i'm the frontend!</h3>
 			<Outlet context={{ user }} />
 			<Footer />
 		</main>
