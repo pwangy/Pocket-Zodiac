f80f1166c3d428bab4d19fbac0d09c47dbc3fab7
pwangy <blipsandclicks@gmail.com>
2024-05-16 12:46:05 -0700

    explore components

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -10,25 +10,25 @@ const Container = ({ data, type }) => {
 	let listItems
 	switch (type) {
 		case 'east':
-            listItems = data?.slice(0, 12).map((e) => <Card key={e.id} type={type} name_12={e.name12}{...e} />)
+            listItems = data?.slice(0, 12).map((e) => <Card className='card' key={e.id} type={type} name_12={e.name12}{...e} />)
             break
         case 'west':
-            listItems = data?.map((e) => <Card key={e.id} type={type} name={e.name} {...e} />)
+            listItems = data?.map((e) => <Card key={e.id} className='card' type={type} name={e.name} {...e} />)
             break
         case 'elements':
-            listItems = data?.map((e) => <Card key={e.id} type={type} name={e.name} {...e} />)
+            listItems = data?.map((e) => <Card key={e.id} className='card' type={type} name={e.name} {...e} />)
             break
         default:
             listItems = null
     }
 
     return (
-        <>
-            <p>{typeNames[type]}</p>
-            <article className='row-wrap'>
+        <section>
+            <h3 className='exp'>{typeNames[type]}</h3>
+            <article className='row-wrap list'>
                 {listItems}
             </article>
-        </>
+        </section>
 )}
 
 export default Container
